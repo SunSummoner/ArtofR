ad1de2860dfb6bcfa11310cca0f419f2f55a1fa5
Kritika Verma <95202116+SunSummoner@users.noreply.github.com>
2023-02-21 22:55:24 +0530

    Made changes in the syntax

--- newfile.r
+++ newfile.r
@@ -66,106 +66,98 @@ print($b)
 #"b" "c" "d" "e"
 
 
-$a
-[1] -0.3495879  0.1215598 -0.3322398
+print($a
+#-0.3495879  0.1215598 -0.3322398
 
-$b
-[1] "b" "c" "d" "e"
+print($b)
+# "b" "c" "d" "e"
 
-> x[1]
-$a
-[1] -0.3495879  0.1215598 -0.3322398
+print(x[1])
+print($a)
+# -0.3495879  0.1215598 -0.3322398
 
-> x[[1]]
-[1] -0.3495879  0.1215598 -0.3322398
-> #single brackets we get list
-> #double brackets we get element
-> x["a"]
-$a
-[1] -0.3495879  0.1215598 -0.3322398
+print(x[[1]])
+# -0.3495879  0.1215598 -0.3322398
+#single brackets we get list
+#double brackets we get element
+print(x["a"])
+print($a)
+# -0.3495879  0.1215598 -0.3322398
 
-> x$a
-[1] -0.3495879  0.1215598 -0.3322398
-> names(x)= c("a", "letters", "c")
-> x$letters
-[1] "b" "c" "d" "e"
-> x$let
-[1] "b" "c" "d" "e"
-> names(x)= c("a", "letters", "letter2")
-> x$let
-NULL
-> x["letters"]
-$letters
-[1] "b" "c" "d" "e"
+print(x$a)
+# -0.3495879  0.1215598 -0.3322398
+names(x)= c("a", "letters", "c")
+print(x$letters)
+#"b" "c" "d" "e"
+print(x$let)
+# "b" "c" "d" "e"
+names(x)= c("a", "letters", "letter2")
+print(x$let)
+#NULL
+print(x["letters"])
+print($letters)
+# "b" "c" "d" "e"
 
-> as.list(1:3)
-[[1]]
-[1] 1
+print(as.list(1:3))
 
-[[2]]
-[1] 2
 
-[[3]]
-[1] 3
+x = list(rnorm(3), 3:9)
+print(x)
+#[[1]]
+#[1] -1.7178191  0.2615907 -0.7782994
 
-> x = list(rnorm(3), 3:9)
-> x
-[[1]]
-[1] -1.7178191  0.2615907 -0.7782994
+#[[2]]
+#[1] 3 4 5 6 7 8 9
 
-[[2]]
-[1] 3 4 5 6 7 8 9
+lapply(x, mean)
+#[[1]]
+#[1] -0.7448426
 
-> lapply(x, mean)
-[[1]]
-[1] -0.7448426
+#[[2]]
+#[1] 6
 
-[[2]]
-[1] 6
 
-> unlist(lapply(list, function))
-Error: unexpected ')' in "unlist(lapply(list, function)"
-> 
-> unlist(lapply(x, mean))
-[1] -0.7448426  6.0000000
-> #simplyfying and converting into vectore
-> #simplyfying and converting into vector
-> sapply(x, mean)
-[1] -0.7448426  6.0000000
-> x = data.frame(sex =c("M","M","F"), age=c(32,34,29))
-> x
-  sex age
-1   M  32
-2   M  34
-3   F  29
-> x$sex
-[1] "M" "M" "F"
-> x[1, "sex"]
-[1] "M"
-> x
-  sex age
-1   M  32
-2   M  34
-3   F  29
-> sapply(x, class) 
-        sex         age 
-"character"   "numeric" 
-> as.matrix(x)
-     sex age 
-[1,] "M" "32"
-[2,] "M" "34"
-[3,] "F" "29"
-> #character matrix
-> as.list(x)
-$sex
-[1] "M" "M" "F"
+ unlist(lapply(x, mean))
+#[1] -0.7448426  6.0000000
+#simplyfying and converting into vectore
+#simplyfying and converting into vector
+sapply(x, mean)
+#[1] -0.7448426  6.0000000
+x = data.frame(sex =c("M","M","F"), age=c(32,34,29))
+print(x)
+#  sex age
+#   M  32
+#2   M  34
+#3   F  29
+      
+print(x$sex)
+#[1] "M" "M" "F"
+print(x[1, "sex"])
+#[1] "M"
+print(x)
+# sex age
+#1   M  32
+#2   M  34
+#3   F  29
+sapply(x, class) 
+ #       sex         age 
+#"character"   "numeric" 
+as.matrix(x)
+#     sex age 
+#[1,] "M" "32"
+#[2,] "M" "34"
+#[3,] "F" "29"
+#character matrix
+as.list(x)
+print($sex)
+#[1] "M" "M" "F"
 
-$age
-[1] 32 34 29
+print($age)
+#[1] 32 34 29
 library(methods)
 as(x, "matrix")
-     sex age 
-[1,] "M" "32"
-[2,] "M" "34"
-[3,] "F" "29"
-> 
+#    sex age 
+#[1,] "M" "32"
+#[2,] "M" "34"
+#[3,] "F" "29"
+ 
