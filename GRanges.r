c32b604d505510ab8e73251dfac63fd5b34363bf
Kritika Verma <95202116+SunSummoner@users.noreply.github.com>
2023-02-23 23:38:24 +0530

    Added some examples

--- GRanges.r
+++ GRanges.r
@@ -1,1 +1,11 @@
+irl <- IRanges(start = c(1,3,5), end = c(3,5,7))
+print (irl)
 
+irl <- IRanges(start = c(1,3,5), width = 3)
+print (irl)
+#both will give the same result
+start(irl)
+width(ir2)<- 1
+print(ir2)
+names(irl) <- paste("A", 1:3, sep="")
+print(irl)
